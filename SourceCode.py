72fcfca124970149c6b9bb1c831672260d3693a5
retr0111 <retrac.min@gmail.com>
2024-01-29 22:27:08 -0700

    Create SourceCode.py

---
+++ SourceCode.py
@@ -0,0 +1,8 @@
+"""
+Project: Github  Repository
+Author: Carter Minster
+Explanation this is the first page for the repository called CS399
+"""
+
+source_code = "Well this is the start of my github experience"
+print(source_code)
